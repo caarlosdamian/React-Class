04f63a1909d9b217ee4320fe03b3b941dbedf691
caarlosdamian <dapecarlos@gmail.com>
2021-07-14 21:38:15 -0500

    add functions to AddComponent

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
 
 export const GifExpertApp = ({ defaultCategories = [] }) => {
   const [categories, setCategories] = useState(defaultCategories);
-
+  console.log(categories)
   return (
     <>
       <h2>GifExpertApp</h2>
       <hr />
+      <AddCategory setCategories={setCategories}/>
       <ol>
           {
               categories.map((categorie)=>(
